216c4aeed193254b8eab3e6db13e7da4fc156f5a
Raquel <raquel.lima.124@ufrn.edu.br>
2024-06-26 22:42:44 -0300

    feat: Update dataAccessRoutes to include Swagger documentation

--- src/routes/dataAccessRoutes.ts
+++ src/routes/dataAccessRoutes.ts
@@ -3,6 +3,32 @@ import { getResource } from "../controllers/dataRepositoryController";
 
 const router = Router();
 
-router.post("/resource", getResource);
+/**
+ * @swagger
+ * /api/resource:
+ *   post:
+ *     tags:
+ *       - Gestor
+ *     summary: Busca dados de um recurso
+ *     description: Este endpoint recupera dados para um recurso dos repositórios CKAN, DKAN ou SOCRATA.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Resource'
+ *           example: 
+ *              repository:"CKAN"
+ *     responses:
+ *       200:
+ *         description: Dados recuperados com sucesso.
+ *       400:
+ *         description: Repositório não informado.
+ *       404:
+ *         description: Repositório não encontrado.
+ *       500:
+ *         description: Erro interno de servidor.
+ */
+router.post('/resource', getResource);
 
 export default router;
